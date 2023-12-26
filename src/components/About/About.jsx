fba73e8b5da467b0553d3aee99f12aab8bb0969a
Juan Carlos Padillo <juancarlospadillo23@gmail.com>
2023-12-26 08:42:03 +0800

    Added animations to AboutSkills component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ import AboutSkills from "./AboutSkills";
 
 const About = () => {
   return (
-    <div className=" flex  z-[1] h-full relative | xl:h-[100dvh] xl:flex xl:justify-center xl:items-center">
+    <div className=" flex  z-[1] h-full relative overflow-hidden | xl:h-[100dvh] xl:flex xl:justify-center xl:items-center">
       <div className=" bg-black/50 backgroundOverlay z-[2]"></div>
       <div className="about_container"></div>
       <div className="mt-10 grid grid-rows-2 z-10 transition text-xs px-5 | sm:px-10 sm:mt-10 | md:px-20 md:w-[71.875rem] md:mt-10 md:place-content-center | xl:px-0 xl:w-[71.875rem] xl:place-content-center ">
--- src/components/About/AboutSkills.jsx
+++ src/components/About/AboutSkills.jsx
@@ -6,9 +6,18 @@ import { useRef } from "react";
 const appearAnimation = {
   hidden: {
     opacity: 0,
-    // x: 1000, TODO:fixed later
+    x: 1000,
   },
   appear: {
+    opacity: 1,
+    x: 0,
+  },
+
+  hideRate: {
+    opacity: 0,
+    x: -500,
+  },
+  showRate: {
     opacity: 1,
     x: 0,
   },
@@ -35,13 +44,22 @@ const AboutSkills = () => {
             <div className=" flex w-24 h-full justify-center items-center bg-orange-700 z-[2] | xl:text-sm">
               {skill.label}
             </div>
+
             <span
+              initial="hideRate"
+              animate={inView ? "showRate" : "hideRate"}
+              transition={{ delay: 10 }}
               className="skills_progressbar h-full bg-accent-clr z-[2]"
               style={{ width: skill.style }}
             ></span>
-            <div className=" text-gray-700 absolute h-full w-full px-2 font-bold bg-transparent z-[1]  flex items-center justify-end | xl:text-sm ">
-              {skill.rate}
-            </div>
+            <motion.div
+              // initial="hideRate"
+              // animate={inView ? "showRate" : "hideRate"}
+              // transition={{ delay: 5 }}
+              className=" text-gray-700 absolute h-full w-full px-2 font-bold bg-transparent z-[1]  flex items-center justify-end | xl:text-sm "
+            >
+              <motion.p>{skill.rate}</motion.p>
+            </motion.div>
           </motion.div>
         ))}
       </motion.div>
